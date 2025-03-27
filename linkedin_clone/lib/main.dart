86614091268974a315aaf1bfe42d73d643b98906
omarrkaddah <omar.akaddah@gmail.com>
2025-03-27 09:10:15 +0200

    removed unused imports

--- linkedin_clone/lib/main.dart
+++ linkedin_clone/lib/main.dart
@@ -1,44 +1,35 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:linkedin_clone/features/connections/data/datasources/connections_remote_data_source.dart';
-import 'package:linkedin_clone/features/connections/data/repository/connections_list_repository_impl.dart';
-import 'package:linkedin_clone/features/connections/domain/usecases/get_connections_usecase.dart';
-import 'package:linkedin_clone/features/connections/domain/usecases/remove_connection_usecase.dart';
-import 'package:linkedin_clone/features/connections/presentations/pages/connections_page.dart';
-import 'package:provider/provider.dart';
-import 'package:linkedin_clone/features/connections/presentations/provider/connections_provider.dart';
-import 'core/themes/app_theme.dart';
 import 'package:http/http.dart' as http;
 import 'package:linkedin_clone/core/Navigation/app_router.dart';
 import 'package:linkedin_clone/features/authentication/Data/Data_Sources/auth_remote_data_source.dart';
 import 'package:linkedin_clone/features/authentication/Data/Data_Sources/auth_remote_data_source_interface.dart';
 import 'package:linkedin_clone/features/authentication/Data/Data_Sources/mock_auth_remote_data_source.dart';
+import 'package:linkedin_clone/features/authentication/Data/Repository/auth_repository_impl.dart';
 import 'package:linkedin_clone/features/authentication/Domain/Repository/auth_repository.dart';
 import 'package:linkedin_clone/features/authentication/Domain/UseCases/forgot_password_usecase.dart';
+import 'package:linkedin_clone/features/authentication/Domain/UseCases/login_usecase.dart';
 import 'package:linkedin_clone/features/authentication/Domain/UseCases/register_usecase.dart';
 import 'package:linkedin_clone/features/authentication/Domain/UseCases/resend_email_usecase.dart';
 import 'package:linkedin_clone/features/authentication/Presentation/Provider/auth_provider.dart';
 import 'package:linkedin_clone/features/authentication/Presentation/Provider/register_provider.dart';
-import 'package:provider/provider.dart';
-import 'core/themes/app_theme.dart';
-import 'package:linkedin_clone/features/authentication/Domain/UseCases/login_usecase.dart';
-import 'package:linkedin_clone/features/authentication/Data/Repository/auth_repository_impl.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:linkedin_clone/features/connections/data/datasources/connections_remote_data_source.dart';
+import 'package:linkedin_clone/features/connections/data/repository/connections_list_repository_impl.dart';
+import 'package:linkedin_clone/features/connections/domain/usecases/get_connections_usecase.dart';
+import 'package:linkedin_clone/features/connections/domain/usecases/remove_connection_usecase.dart';
+import 'package:linkedin_clone/features/connections/presentations/provider/connections_provider.dart';
 import 'package:linkedin_clone/features/feed/domain/usecases/save_post_usecase.dart';
 import 'package:provider/provider.dart';
-import 'package:dio/dio.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
 
 import '../features/feed/data/data_sources/feed_remote_data_source.dart';
 import '../features/feed/data/repositories/feed_repository_impl.dart';
-import '../features/feed/domain/usecases/get_posts_usecase.dart';
 import '../features/feed/domain/usecases/create_post_usecase.dart';
-import '../features/feed/presentation/pages/feed_page.dart';
-import '../features/feed/presentation/pages/create_post_page.dart';
-import '../features/feed/presentation/provider/feed_provider.dart';
 import '../features/feed/domain/usecases/delete_post_usecase.dart';
-import '../features/feed/domain/usecases/react_to_post_usecase.dart';
-import 'package:linkedin_clone/core/themes/app_theme.dart';
-import 'package:linkedin_clone/features/company/presentation/screens/company_profile_screen.dart';
+import '../features/feed/domain/usecases/get_posts_usecase.dart';
+import '../features/feed/presentation/provider/feed_provider.dart';
+import 'core/themes/app_theme.dart';
 
 void main() {
 
