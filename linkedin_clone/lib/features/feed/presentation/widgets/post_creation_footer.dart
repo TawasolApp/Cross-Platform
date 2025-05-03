930829ced5b11d3c3603826343a0ba7bfd28e043
nournaderr <nournader118@gmail.com>
2025-05-03 15:45:17 +0300

    upload document

--- linkedin_clone/lib/features/feed/presentation/widgets/post_creation_footer.dart
+++ linkedin_clone/lib/features/feed/presentation/widgets/post_creation_footer.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import '../../../../core/utils/upload_image.dart';
+import 'package:file_picker/file_picker.dart';
+import '../../../company/domain/repositories/media_repository.dart';
+import 'package:provider/provider.dart';
 
 class PostCreationFooter extends StatelessWidget {
   final void Function(String imageUrl) onImageUploaded;
@@ -41,6 +44,33 @@ class PostCreationFooter extends StatelessWidget {
               } catch (e) {
                 print('Upload failed: $e');
               }
+            }
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.description_outlined),
+          onPressed: () async {
+            final result = await FilePicker.platform.pickFiles(
+              type: FileType.custom,
+              allowedExtensions: ['pdf', 'doc', 'docx'],
+            );
+
+            if (result != null && result.files.single.path != null) {
+              final path = result.files.single.path!;
+              final pickedFile = XFile(path);
+              try {
+                final mediaRepo = Provider.of<MediaRepository>(
+                  context,
+                  listen: false,
+                );
+                final url = await mediaRepo.uploadDocument(pickedFile);
+                onImageUploaded(url);
+                print('📄 Document uploaded to: $url');
+              } catch (e) {
+                print('❌ Document upload failed: $e');
+              }
+            } else {
+              print('⚠️ No document selected');
             }
           },
         ),
